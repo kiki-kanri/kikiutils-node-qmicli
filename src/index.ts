af2974712d8c94621efebc4cbeae78604254cc00
kiki-kanri <a470666@gmail.com>
2023-08-15 15:13:18 +0800

    Set index.ts export all classes and functions.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-import { readdir } from 'fs-extra';
-
-import Device from './classes/device';
-
-export const listDevices = async () => {
-	const devices = await readdir('/dev') as string[];
-	const filteredDevices = devices.filter((deviceName) => deviceName.includes('cdc-'));
-	return filteredDevices.map((deviceName) => new Device(deviceName));
-}
+export * from './classes';
+export * from './functions';
